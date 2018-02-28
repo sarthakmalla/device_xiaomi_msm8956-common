e5f8f32367009eb32714bebb00165017a0e74e8a
nijel8 <nijel8@gmail.com>
2018-02-28 13:28:19 -0500

    camera: Fix build for 8.1
    *<system/window.h> is no more

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -114,7 +114,7 @@ LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
         hardware/qcom/display-caf/msm8952/libqservice
-LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
+LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
 ifeq ($(TARGET_TS_MAKEUP),true)
